862d355efd46eb63c8fba90079d9eac783b897ea
J-Pard <pardons.jonathan@gmail.com>
2020-07-01 12:04:50 +0200

    Running

--- components/App.js
+++ components/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import Timer from './timer';
-import Controller from './controller';
 
 const App = () => {
-  return (
-        <main>
-            <div>
-                  <h1>React Pomodoro</h1>
-            </div>
-            
-            <div className="count">
+
+      return (
+            <main>
+                  <div>
+                        <h1>React Pomodoro</h1>
+                  </div>
+                  
                   <Timer/>
-                  <Controller/>
-            </div>
-      </main>
-  );
+
+            </main>
+      );
 };
 
 export default App;
--- components/Timer.jsx
+++ components/Timer.jsx
@@ -2,9 +2,53 @@ import React, { useState } from 'react';
 
 const Timer = () => {
 
+      const defaultTime = 10;
+      const [time, setTime] = useState(defaultTime); // seconds
+
+      let timer = time;
+      let minutes;
+      let seconds;
+      let check;
+
+      const addTime = () => {
+            setTime(time + 60);
+      };
+
+      const reduceTime = () => {
+            setTime(time - 60);
+      };
+
+      const resetTime = () => {
+            setTime(defaultTime);
+      };
+
+      const startTimer = () => {
+                  setInterval(() => {
+                        timer--;
+                        setTime(timer);
+                  }, 1000);               
+      };
+
+      const showTime = (timer) => {
+            minutes = Math.floor(timer/60);
+            minutes = minutes < 10 ? `0${minutes}` : minutes;
+            seconds = timer - (minutes*60);
+            seconds = seconds < 10 ? `0${seconds}` : seconds;
+            return `${minutes} : ${seconds}`;
+      }
+
       return (
             <div className="timer">
-                  20:00
+                  <div className="time">
+                        {showTime(timer)}
+                  </div>
+
+                  <div className="controller-btn">
+                        <button className="btn" onClick={addTime}>+</button>
+                        <button className="btn" onClick={resetTime}>Reset</button>
+                        <button className="btn" onClick={startTimer}>Start</button>
+                        <button className="btn" onClick={reduceTime}>-</button>
+                  </div>
             </div>
       );
 };
